88c21f7df1501a21f36b4bfeffc16f14e6c14498
VinceLim68 <1500725439@qq.com>
2017-04-30 22:33:35 +0800

    remove 文件夹-过期文件

--- 过期文件/_replace.py
+++
@@ -1,6 +0,0 @@
-import datetime
-a = "2012-05-01 23:59:59.0"
-a = a.replace('.0','')
-# print type(datetime.date.today())
-b = datetime.datetime.strptime(a, "%Y-%m-%d %H:%M:%S").date()
-print type(b)
--- 过期文件/text_mysql.py
+++
@@ -1,3 +0,0 @@
-import MySQLdb
-db = MySQLdb.connect(host = "192.168.1.207",user = "root",passwd = "root",port = 3306,db = "property_info",charset = "utf8")
-db.close()
