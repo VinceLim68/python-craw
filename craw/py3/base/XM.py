3d22b3b2a7f5a6dab0b6aaa23704f40157b088e9
VinceLim68 <1500725439@qq.com>
2017-11-13 07:58:27 +0800

    2017/11/13备份

--- craw/py3/base/XM.py
+++ craw/py3/base/XM.py
@@ -5,22 +5,22 @@ XM = MassController.MassController(XmhousePage.XmhousePage)
 XM.craw_controller(url)
 XM.outputer.out_mysql()
 
-# url = ['http://xm.esf.leju.com/house']
-# LEJV = MassController.MassController(LejvPage.LejvPage)
-# LEJV.delay = 3
-# LEJV.headers = dict(Host="xm.esf.leju.com", Origin="http://xm.esf.leju.com", Referer="http://xm.esf.leju.com/house/")
-# LEJV.craw_controller(url)
-# LEJV.outputer.out_mysql()
+url = ['http://xm.esf.leju.com/house']
+LEJV = MassController.MassController(LejvPage.LejvPage)
+LEJV.delay = 3
+LEJV.headers = dict(Host="xm.esf.leju.com", Origin="http://xm.esf.leju.com", Referer="http://xm.esf.leju.com/house/")
+LEJV.craw_controller(url)
+LEJV.outputer.out_mysql()
 
-# url = ['http://xm.ganji.com/fang5/o2/']
-# GJ = MassController.MassController(GanjiPage.GanjiPage)
-# GJ.delay = 3
-# GJ.headers = dict(Host="xm.ganji.com",  Referer="http://xm.ganji.com/fang5/o2/")
-# GJ.craw_controller(url)
-# GJ.outputer.out_mysql()
+url = ['http://xm.ganji.com/fang5/o2/']
+GJ = MassController.MassController(GanjiPage.GanjiPage)
+GJ.delay = 3
+GJ.headers = dict(Host="xm.ganji.com",  Referer="http://xm.ganji.com/fang5/o2/")
+GJ.craw_controller(url)
+GJ.outputer.out_mysql()
 
-# url = ['http://xm.maitian.cn/esfall/PG2']
-# MT = MassController.MassController(MaitianPage.MaitianPage)
-# MT.headers = dict(Host="xm.maitian.cn")
-# MT.craw_controller(url)
-# MT.outputer.out_mysql()
+url = ['http://xm.maitian.cn/esfall/PG2']
+MT = MassController.MassController(MaitianPage.MaitianPage)
+MT.headers = dict(Host="xm.maitian.cn")
+MT.craw_controller(url)
+MT.outputer.out_mysql()
